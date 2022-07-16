61514c95d3dc08252d739ef01bf74b5de06a65b6
stefanus_yudi_irwan <yudi.stefanus22@gmail.com>
2022-07-16 12:57:34 +0800

    membuat logic fizzbuzz dengan python

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/home/st_yudi/programs/anaconda3/bin/python3
+
+# menerima input angka dari user
+n = int(input("Masukan input angka yang diinginkan : "))
+
+# loop dari 1 - n mencetak fizzbuzz
+for i in range(1,n+1):
+    if i%15==0:
+        print("fizzbuzz")
+    elif i%3==0:
+        print("fizz")
+    elif i%5==0:
+        print("buzz")
+    else:
+        print(str(i))
